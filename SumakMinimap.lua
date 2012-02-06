944f8a1179e8bfa191b709bdd338b03b7777ba51
Sumak <sumak.serge.m@gmail.com>
2012-02-06 21:16:44 +0200

    [1.3.R000] Добавление rightclick menu

--- SumakMinimap.lua
+++ SumakMinimap.lua
@@ -9,6 +9,10 @@ local anchor_point = 'TOPRIGHT'
 local x = -25
 local y = -40
 
+Minimap : SetScale(1)
+Minimap : SetPlayerTexture (CFG.media.minimaparrow)
+Minimap : SetPlayerTextureHeight(36)
+Minimap : SetPlayerTextureWidth(36)
 FCV.backdrop = {
 	bgFile = CFG.media.bgfile,
 	--edgeFile = CFG.media.blank,
@@ -84,7 +88,7 @@ local coord_Update = function (self, t)
 	ela = .2
 end
 
-m_coord : SetScript("OnUpdate",coord_Update)
+m_coord : SetScript("OnUpdate", coord_Update)
 -------------------------------------------------
 	
 -------------------------------------------------
@@ -258,6 +262,59 @@ Kill(GameTimeFrame)
 --MiniMapTracking:Hide()
 
  --/framestack - команда отображения фреймов!!!!
+ 
+------------------------------------------	
+-- Right click menu
+------------------------------------------
+local menuFrame = CreateFrame("Frame", "MinimapRightClickMenu", UIParent, "UIDropDownMenuTemplate")
+local menuList = {
+    {text = CHARACTER_BUTTON,
+    func = function() ToggleCharacter("PaperDollFrame") end},
+    {text = SPELLBOOK_ABILITIES_BUTTON,
+    func = function() ToggleFrame(SpellBookFrame) end},
+    {text = TALENTS_BUTTON,
+    func = function() ToggleTalentFrame() end},
+    {text = ACHIEVEMENT_BUTTON,
+    func = function() ToggleAchievementFrame() end},
+    {text = QUESTLOG_BUTTON,
+    func = function() ToggleFrame(QuestLogFrame) end},
+    {text = SOCIAL_BUTTON,
+    func = function() ToggleFriendsFrame(1) end},
+	{text = PLAYER_V_PLAYER,
+	func = function() ToggleFrame(PVPFrame) end},
+    {text = LFG_TITLE,
+    func = function() ToggleFrame(LFDParentFrame) end},
+    {text = L_LFRAID,
+    func = function() ToggleFrame(LFRParentFrame) end},
+    {text = HELP_BUTTON,
+    func = function() ToggleHelpFrame() end},
+    {text = "Календарь", 
+    func = function()
+    if(not CalendarFrame) then LoadAddOn("Blizzard_Calendar") end
+        Calendar_Toggle()
+    end},
+}
+
+local addonmenu = {
+	{text = "AtlasLoot",
+    func = function() if IsAddOnLoaded("AtlasLoot") then AtlasLootDefaultFrame:Show() end end},
+	{text = "Skada",
+    func = function() if IsAddOnLoaded("Skada") then Skada:ToggleWindow() end end},
+	{text = "WIM",
+    func = function() if IsAddOnLoaded("WIM") then WIM.ShowAllWindows() end end},
+	{text = "Omen",
+    func = function() if IsAddOnLoaded("Omen") then Omen:Toggle() end end},
+	{text = "Recount",
+    func = function() if IsAddOnLoaded("Recount") then Recount.MainWindow:Show() end end},
+}
+
+Minimap : SetScript("OnMouseUp", function(self, button)
+	if button == "RightButton" and not InCombatLockdown() then
+		EasyMenu(menuList, menuFrame, "cursor", 0, 0, "MENU", 2)
+	else
+		Minimap_OnClick(self)
+	end
+end)
  
 SLASH_RESETMINIMAP1 = "/rmmp"
 SLASH_RESETMINIMAP1 = "/resetmmp"
